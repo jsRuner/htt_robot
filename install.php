5a880da020b79ea997d2b031321261b30a264353
吴文付 <wwf@boluo361.com>
2017-06-11 22:43:32 +0800

    删除不必要文件。更新sql语句

--- install.php
+++ install.php
@@ -8,7 +8,8 @@
 if(!defined('IN_DISCUZ')) {
 	exit('Access Denied');
 }
-runquery("CREATE TABLE IF NOT EXISTS `cdb_httrobot_message` (
+runquery("
+CREATE TABLE `pre_httrobot_message` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `uid` int(11) NOT NULL COMMENT '用户Id',
   `username` varchar(125) NOT NULL COMMENT '用户姓名',
@@ -16,8 +17,20 @@ runquery("CREATE TABLE IF NOT EXISTS `cdb_httrobot_message` (
   `dateline` int(11) NOT NULL COMMENT '时间',
   `message` text NOT NULL COMMENT '用户的提问',
   `reply` text NOT NULL COMMENT '机器人的回答',
+  `assess` tinyint(1) DEFAULT '0' COMMENT '0未评价1满意2不满意',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM;");
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+");
+
+runquery("
+CREATE TABLE `pre_httrobot_question` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `question` varchar(125) NOT NULL DEFAULT '' COMMENT '问题的标题',
+  `answer` varchar(1000) NOT NULL DEFAULT '' COMMENT '问题的答案',
+  `dateline` datetime NOT NULL COMMENT '添加时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+");
 
 
 
